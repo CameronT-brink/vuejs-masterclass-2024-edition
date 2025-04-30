a45e9ce8ff4312cbf42c9fdaebc8915cad4ff7bd
Cameron Thomas <CThomas@brinkleyrv.com>
2025-04-30 07:46:06 -0400

    section 2 video 2 updates in progress

---
+++ vuejs-masterclass-2025/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <h1>Home Page</h1>
+        <a href="/projects"></a>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
